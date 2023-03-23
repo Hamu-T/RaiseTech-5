e49d86c9a372aacd88f052c67fc3c95d2fedf742
筒井公太郎 <newworld@tsutsuikoutarounoMacBook-Air.local>
2023-03-23 12:54:46 +0900

    first-commit

---
+++ style.js
@@ -0,0 +1,10 @@
+document.querySelector('.js-hamburger').addEventListener(
+'click',
+() => {
+document. querySelector('.js-hamburger').classList.toggle ('menu-open');
+document. querySelector ('.p-gmenu').classList.toggle('menu-open');
+document. querySelector ('body').classList.toggle('menu-open');
+document. querySelector('.header-nav').classList.toggle ('menu-open');
+}
+);
+
