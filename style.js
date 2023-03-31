288dad4f32e2410026e372802ef9aadac6601ec0
筒井公太郎 <newworld@tsutsuikoutarounoMacBook-Air.local>
2023-03-31 11:02:29 +0900

    btn-animation-ok

--- style.js
+++ style.js
@@ -7,11 +7,9 @@ document. querySelector('.js-hamburger').classList.toggle ('menu-open');
 document. querySelector ('.p-gmenu').classList.toggle('menu-open');
 document. querySelector ('body').classList.toggle('menu-open');
 document. querySelector('.header-nav').classList.toggle ('menu-open');
-}
-);
+});
 
-
-
+    
 // サブメニューの表示・非表示
 
 $(function() {
@@ -20,18 +18,5 @@ $(function() {
         
         var elementHeight = $('.sub-menu').height();
         console.log(elementHeight);
-        // 👆Uncaught TypeError: $(...).chidlren is not a function
-        // 👆Uncaught TypeError: $(...).chidlren は関数ではありません。
     });
 });
-
-
-// chatGPT 「 css: .sub-menu{display:none;} 」<- 付けてアニメーションする(出来なかった)
-
-// $(function() {
-//     $(".menu").hover(function() {
-//         $(this).siblings(".sub-menu").stop().slideDown(2000);
-//     }, function() {
-//         $(this).siblings(".sub-menu").stop().slideUp(2000);
-//     });
-// });
